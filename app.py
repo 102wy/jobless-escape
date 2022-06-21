2e974dbbcb25b46fb9cfb1fa8314bb73e54bd159
yougeun6021 <dbrms1a1a@naver.com>
2022-06-21 15:13:47 +0900

    잡코리아사이트의 스프링,리액트  검색어에 해당하는 페이지의 회사이름,설명,고용분야,고용,고용형태,지역,만료일을 크롤링하여 db에 저장

--- app.py
+++ app.py
@@ -16,6 +16,65 @@ PW = os.environ.get('DB_PW')
 client = MongoClient("mongodb+srv://"+ID+":"+PW+"@joblessescape.dvnaltz.mongodb.net/?retryWrites=true&w=majority")
 db = client.joblessescape
 
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+react_data = requests.get("https://www.jobkorea.co.kr/Search/?stext=react&careerType=1",headers=headers)
+spring_data = requests.get("https://www.jobkorea.co.kr/Search/?stext=spring&careerType=1",headers=headers)
+
+soup_react = BeautifulSoup(react_data.text, 'html.parser')
+soup_spring = BeautifulSoup(spring_data.text, 'html.parser')
+
+
+job_announcements_react = soup_react.select('#content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li')
+job_announcements_spring = soup_spring.select('#content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li')
+
+
+
+for job_announcement_react in job_announcements_react:
+    company_name = job_announcement_react.select_one('div > div.post-list-corp > a').text
+    info = job_announcement_react.select_one('div > div.post-list-info > a').text.strip()
+    job = job_announcement_react.select_one('div > div.post-list-info > p.etc').text
+    exp = job_announcement_react.select_one('div > div.post-list-info > p.option > span.exp').text
+    region = job_announcement_react.select_one('div > div.post-list-info > p.option > span.loc.long').text
+    date = job_announcement_react.select_one('div > div.post-list-info > p.option > span.date').text
+
+    doc = {
+        'company_name':company_name,
+        'info': info,
+        'job': job,
+        'exp': exp,
+        'region': region,
+        'date': date
+    }
+    db.job_announcement_react.insert_one(doc)
+
+
+for job_announcement_spring in job_announcements_spring:
+    company_name = job_announcement_spring.select_one('div > div.post-list-corp > a').text
+    info = job_announcement_spring.select_one('div > div.post-list-info > a').text.strip()
+    job = job_announcement_spring.select_one('div > div.post-list-info > p.etc').text
+    exp = job_announcement_spring.select_one('div > div.post-list-info > p.option > span.exp').text
+    region = job_announcement_spring.select_one('div > div.post-list-info > p.option > span.loc.long').text
+    date = job_announcement_spring.select_one('div > div.post-list-info > p.option > span.date').text
+
+    doc = {
+        'company_name': company_name,
+        'info': info,
+        'job': job,
+        'exp': exp,
+        'region': region,
+        'date': date
+    }
+    db.job_announcement_spring.insert_one(doc)
+
+    
+
+
+
+
+
+
+
+
 
 @app.route('/', methods=["GET"])
 def home():
