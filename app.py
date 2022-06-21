8c7eac8b07552a5461de269df7998b388c7ba039
102wy <tser5@naver.com>
2022-06-21 17:18:48 +0900

    quiz update

--- app.py
+++ app.py
@@ -20,9 +20,21 @@ db = client.joblessescape
 def home():
     return render_template('index.html')
 
-@app.route('/quiz', methods=["GET"])
-def quiz():
+@app.route('/quiz/<index>', methods=["GET"])
+def quiz(index):
+    quiz_list = list(db.quiz.find({}, {'_id': False}))
+    quiz_index = db.quiz.find_one({'id': index})['id']
+    quiz_content = db.quiz.find_one({'id': index})['quiz']
+    quiz_answer = db.quiz.find_one({'id': index})['quiz_answer']
+
+    return render_template('quiz.html', quiz_list=quiz_list, quiz_index=quiz_index, quiz_content=quiz_content, quiz_answer=quiz_answer)
+
+@app.route('/quiz/<index>', methods=["POST"])
+def checking(index):
+    quiz_list = list(db.quiz.find({}, {'_id': False}))
+
     return render_template('quiz.html')
 
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=4000, debug=True)
--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,33 +1,66 @@
 window.onload = function (){
 
-    let time = 0;
-    let hour = 0;
-    let min = 0;
-    let sec = 0;
-    let timer;
+    // timer start
 
-    const countUp = () => {
-        timer = setInterval(()=>{
-            time ++;
-            min = Math.floor(time/60);
-            hour = Math.floor(min/60);
-            sec = time%60;
-            min = min%60;
+    // let time = 0;
+    // let hour = 0;
+    // let min = 0;
+    // let sec = 0;
+    // let timer;
+    //
+    // const countUp = () => {
+    //     timer = setInterval(()=>{
+    //         time ++;
+    //         min = Math.floor(time/60);
+    //         hour = Math.floor(min/60);
+    //         sec = time%60;
+    //         min = min%60;
+    //
+    //         let th = hour;
+    //         let tm = min;
+    //         let ts = sec;
+    //         if (th<10){
+    //             th = `0${hour}`
+    //         }
+    //         if (tm<10){
+    //             tm = `0${min}`
+    //         }
+    //         if (ts<10){
+    //             ts = `0${sec}`
+    //         }
+    //         document.querySelector('.timer').innerHTML = `${th}:${tm}:${ts}`;
+    //     },1000);
+    // }
+    // countUp();
 
-            let th = hour;
-            let tm = min;
-            let ts = sec;
-            if (th<10){
-                th = `0${hour}`
+    let downtime = 60;
+    const countDown = () => {
+        setInterval(()=>{
+            downtime --;
+            if(downtime <= 0){
+                document.querySelector('.quiz-modal').style.display = 'flex';
+                downtime = 0;
             }
-            if (tm<10){
-                tm = `0${min}`
+            if(downtime < 10){
+                document.querySelector('.timer').innerHTML = `00:0${downtime}`
+            }else{
+                document.querySelector('.timer').innerHTML = `00:${downtime}`
             }
-            if (ts<10){
-                ts = `0${sec}`
-            }
-            document.querySelector('.timer').innerHTML = `${th}:${tm}:${ts}`;
         },1000);
     }
-    countUp();
+    countDown();
+
+    // input value 가져오기
+    // const quizAnswer = document.querySelector('.quiz-input');
+    // quizAnswer.addEventListener('keypress',(e)=>{
+    //     if(e.key === 'Enter'){
+    //         let quizValue = quizAnswer.value;
+    //         console.log(quizValue);
+    //         quizAnswer.value = '';
+    //     }
+    // });
+
+
+    // timer end
+
 }
