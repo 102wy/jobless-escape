144b33f887151fac41523fdd313e8297a1185568
yougeun6021 <dbrms1a1a@naver.com>
2022-06-21 11:56:45 +0900

    해쉬함수 import 및 중복된 아이디,중복된 닉네임,회원가입시 유저정보 저장 서버 코드 구현

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import hashlib
 import requests
 from bs4 import BeautifulSoup
 
@@ -25,6 +26,37 @@ def home():
 @app.route('/signup',methods=["GET"])
 def signup():
     return render_template('signup_page.html')
+
+
+
+
+@app.route('/sign_up/check_ID', methods=['POST'])
+def check_ID():
+    id_receive = request.form['id_give']
+    exists = bool(db.users.find_one({"id": id_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/sign_up/check_nickname', methods=['POST'])
+def check_nickname():
+    nickname_receive = request.form['nickname_give']
+    exists = bool(db.users.find_one({"nickname": nickname_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def signup_save():
+    id_receive = request.form['id_give']
+    password_receive = request.form['password_give']
+    nickname_receive = request.form['nickname_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "id": id_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "nickname": nickname_receive,  # 프로필 이름 기본값은 아이디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
 
 
 if __name__ == '__main__':
